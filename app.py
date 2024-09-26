2c545cb2adc7a0002a6d4cabca8560f672e2a0df
aziza-zadjali <140299461+aziza-zadjali@users.noreply.github.com>
2024-09-26 23:16:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,14 @@ def main():
 
         # Add the 'Our Team' comment and team.png image after the "Get Started" button
         st.markdown("<h3 style='text-align:center;'>Our Team</h3>", unsafe_allow_html=True)
-        st.image("team.png", width=700)
+        st.markdown(
+            """
+            <div style='display:flex; justify-content:center;'>
+                <img src='team.png' width='700'>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
     if st.session_state.show_main_app:
         # Main app (initially hidden)
