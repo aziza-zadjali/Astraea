1ec470d9051ef51d07343a3d1c1980871b862732
aziza-zadjali <140299461+aziza-zadjali@users.noreply.github.com>
2024-10-07 23:26:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from PIL import Image
 TEMPLATE_DIR = "templates"
 
 def main():
-    st.title('Welcome to Astraea - Your AI Legal Assistant')
+    st.set_page_config(page_title="Astraea - Legal Query Assistant", layout="wide")
 
     # Initialize session state for landing page
     if "show_main_app" not in st.session_state:
